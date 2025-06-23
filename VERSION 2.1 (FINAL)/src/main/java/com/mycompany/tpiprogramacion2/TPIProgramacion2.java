3762968a6e21b2a4f1e9dc41ab41407548971704
Benjaaaaaaaaaaaaaaah <benjaminsalgadobolatti@gmail.com>
2025-06-23 13:54:07 -0300

    tpi

---
+++ VERSION 2.1 (FINAL)/src/main/java/com/mycompany/tpiprogramacion2/TPIProgramacion2.java
@@ -0,0 +1,11 @@
+package com.mycompany.tpiprogramacion2;
+
+import com.mycompany.tpiprogramacion2.controlador.Controlador;
+
+public class TPIProgramacion2 {
+
+    public static void main(String[] args) {
+        Controlador controlador = new Controlador();
+        controlador.ejecutar();
+    }
+}
